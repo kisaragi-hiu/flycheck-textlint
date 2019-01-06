d9582be112f13b584c6733baf5c79b2084366243
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-01-06 18:49:35 +0900

    flycheck-textlint.el with config file variable

---
+++ flycheck-textlint.el
@@ -0,0 +1,46 @@
+;;; flycheck-textlint.el --- Flycheck integration for textlint
+;;; -*- lexical-binding: t; -*-
+
+;; Authors: Rob Stewart <R.Stewart@hw.ac.uk>
+;;          Kisaragi Hiu <mail@kisaragi-hiu.com>
+;; URL: https://gitlab.com/kisaragi-hiu/flycheck-textlint
+;; Version: 1.0.0
+;; Keywords: convenience languages tools
+;; Package-Requires: ((flycheck "0.25"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; Flycheck integration for Textlint.
+
+;;; Code:
+
+(require 'flycheck)
+
+(flycheck-def-option-var
+    flycheck-textlint-config-file
+    (concat (getenv "HOME") "/.config/textlint/textlintrc.json")
+    textlint
+  "Path to textlint config file."
+  :type 'file
+  :safe #'stringp)
+
+(flycheck-define-checker textlint
+  "A linter for textlint."
+  :command ("textlint"
+            "--config" (eval flycheck-textlint-config-file)
+            "--format" "unix"
+            source-inplace)
+  :error-patterns
+  ((warning line-start (file-name) ":" line ":" column ": "
+            (message (one-or-more not-newline)
+                     (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+            line-end))
+  :modes
+  (text-mode markdown-mode))
+
+(add-to-list 'flycheck-checkers 'textlint)
+
+(provide 'flycheck-textlint)
+;;; flycheck-textlint.el ends here
